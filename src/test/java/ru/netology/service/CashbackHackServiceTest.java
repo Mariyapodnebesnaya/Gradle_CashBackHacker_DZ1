4c51c514efaf020f04a40deba4a8391c8b94e6e9
Mariyapodnebesnaya <novikovamanya08@bk.ru>
2022-11-21 11:34:57 +0300

    Created new branch junit4. Added TestNG dependencies.

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,9 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
@@ -12,15 +13,15 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void RemainPurchaseAmountToReceiveBonusesMore1000() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1350;
-        int expected = 650;
+        int amount = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
